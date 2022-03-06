0718ef69fc009c0de0f99166f2890e60fd1fb6d0
Anatoly Zhilyakov <bi0hok@bk.ru>
2022-03-06 23:15:00 +0300

    Fix task-03

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -3,16 +3,14 @@ function evenOdd(arr) {
   let odd = 0;
   let zero = 0;
   for (let i = 0; i < arr.length; i += 1) {
-    if (!isNaN(arr[i])) {
-      if (typeof (arr[i]) === 'number') {
-        if (arr[i] === 0) {
-          zero += 1;
-        } else
-        if ((arr[i] % 2) === 0) {
-          even += 1;
-        } else {
-          odd += 1;
-        }
+    if ((!isNaN(arr[i])) && (typeof (arr[i]) === 'number')) {
+      if (arr[i] === 0) {
+        zero += 1;
+      } else
+      if ((arr[i] % 2) === 0) {
+        even += 1;
+      } else {
+        odd += 1;
       }
     }
   }

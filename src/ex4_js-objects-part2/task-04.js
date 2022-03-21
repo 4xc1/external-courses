1b0a8f3ff3103f20050657c5906a5f4b0b0497a3
Anatoly Zhilyakov <bi0hok@bk.ru>
2022-03-21 14:33:02 +0300

    Fix task-04

--- src/ex4_js-objects-part2/task-04.js
+++ src/ex4_js-objects-part2/task-04.js
@@ -1,6 +1,6 @@
 function capitalLetter(str) {
-  str.toLowerCase();
-  const newStr = str[0].toUpperCase() + str.slice(1);
+  let newStr = str.toLowerCase();
+  newStr = str[0].toUpperCase() + str.slice(1);
   return newStr;
 }
 
